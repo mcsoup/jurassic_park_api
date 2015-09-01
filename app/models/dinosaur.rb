414c8529ed6d9e422ecf81af3d8eb7c30faf809d
Campbell, Matthew (mc304r) <mc304r@us.att.com>
2015-09-01 16:11:42 -0500

    Added validations for dinosaur

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,13 +1,33 @@
 class Dinosaur < ActiveRecord::Base
+  validates :name, presence: true
+  validates :species_id, presence: true
+  validate :valid_cage?
+  
   belongs_to :cage
+  belongs_to :species
+
   self.inheritance_column = :diet
+
+  before_save :update_cage, :if => ->(d) { !d.new_record? && d.cage_id_changed? }
  
   #scope
-  scope :carnivores, -> { where(race: 'Carnivore') } 
-  scope :herbivores, -> { where(race: 'Herbivore') }
+  scope :carnivores, -> { where(diet: 'Carnivore') } 
+  scope :herbivores, -> { where(diet: 'Herbivore') }
 
-  def self.races
+  def self.diets
     %w(Herbivore Carnivore)
   end
 
+  def valid_cage?
+    errors.add(:power_down, "Cage Power is Down") unless cage.nil? or cage.power
+    errors.add(:over_capacity, "Cage Max Capacity has been reached") unless cage.nil? or cage.contained < cage.max_capacity 
+  end
+
+private
+  def update_cage
+    Cage.decrement_counter(:contained, self.cage_id_was)
+    Cage.increment_counter(:contained, self.cage_id)
+  end
 end
+
+
--- test/models/dinosaur_test.rb
+++ test/models/dinosaur_test.rb
@@ -1,7 +1,63 @@
 require 'test_helper'
 
-class DinosaurTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Dinosaur do
+  let(:trex) {Species.create label: 'Tyrannosaurus', diet: 'Carnivore'}
+  let(:carnivore1) {Carnivore.create name: 'Rex', species: trex}
+  let(:carnivore2) {Carnivore.create name: 'Rexy', species: trex}
+  let(:trike) {Species.create label: 'Triceratops', diet: 'Herbivore'}
+  let(:herbivore) {Herbivore.create name: 'Sara', species: trike}
+  let(:herbivore) {Herbivore.create name: 'Sarah', species: trike}
+
+  describe "diets" do
+    it "should return the different diets" do
+      assert_equal Dinosaur.diets, ["Herbivore", "Carnivore"]
+    end
+  end
+
+  describe "counter_cache" do
+    before do
+      @cage = Cage.create(max_capacity: 10, power: true)
+      @carnivore = carnivore1
+    end
+
+    describe "should increase the cage contained" do
+      it "by plus one" do
+        assert_difference('@cage.reload.contained') do
+          @cage.dinosaurs << @carnivore   
+        end
+      end
+
+      it "by minus one" do
+        @cage.dinosaurs << @carnivore
+        @carnivore.update cage_id: nil
+        assert_equal @cage.contained, 0
+      end
+    end
+  end
+
+  describe "valid_cage?" do
+    before do
+      @cage = Cage.create(max_capacity: 10, power: true)
+      @carnivore = carnivore1
+    end
+
+    it "should be valid" do
+      @carnivore.cage = @cage
+      assert @carnivore.valid?
+    end
+
+    describe  "should be invalid" do
+      it "when cage power is down" do
+        @cage.update power: false
+        @carnivore.cage = @cage
+        refute @carnivore.valid?
+      end
+
+      it "when cage is at max capacity" do
+        @cage.update contained: 10
+        @carnivore.cage = @cage
+        refute @carnivore.valid?
+      end
+    end
+  end
 end
